fa83b1933333845d27847a8818af4db1c51512b1
Ghufran <ghufrankhan.gzk@gmail.com>
2020-05-29 21:05:31 +0530

    Added data layer and wrote controller test case for data later.

--- src/test/java/com/gazoul/unittesting/mockitoJunit/ItemControllerTest.java
+++ src/test/java/com/gazoul/unittesting/mockitoJunit/ItemControllerTest.java
@@ -18,6 +18,8 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -32,6 +34,7 @@ public class ItemControllerTest {
 
     @MockBean
     ItemBusinessService businessService;
+
     @Test
     public void dummyItem() throws Exception {
 
@@ -45,12 +48,12 @@ public class ItemControllerTest {
                 .andExpect(content().json("{\"id\":1,\"name\":\"Ball\",\"price\":10}")) // we can add in space or remove some key pairs ... and it will automatically know and
                 .andReturn();                               //
 
-          //JSONAssert.assertEquals(expected, actual, strict);
+        //JSONAssert.assertEquals(expected, actual, strict);
     }
     @Test
     public void itemFromBusinessService() throws Exception {
         when(businessService.retrieveHardcodedItem()).thenReturn(
-                new Item(2,"Item2",10,10));
+                new Item(2, "Item2", 10, 10));
         RequestBuilder request = MockMvcRequestBuilders //
                 .get("/items-from-business-service")        //          Perform a Get call
                 .accept(MediaType.APPLICATION_JSON);    //
@@ -61,6 +64,25 @@ public class ItemControllerTest {
                 .andReturn();                               //
     }
 
+    @Test
+    public void itemFromDataService() throws Exception {
+        when(businessService.retrieveAllItems()).thenReturn(
+                Arrays.asList(
+                        new Item(2, "Item2", 10, 10),
+                        new Item(3, "Item3", 20, 100),
+                        new Item(4, "Item4", 30, 200))
+        );
+        RequestBuilder request = MockMvcRequestBuilders //
+                .get("/all-items-from-database")        //          Perform a Get call
+                .accept(MediaType.APPLICATION_JSON);    //
+
+        MvcResult result = mockMvc.perform(request)         //
+                .andExpect(status().isOk())                 //   Verification of the response
+                .andExpect(content().json("[{id:2,name:Item2,price:10}," +
+                        "{id:3,name:Item3,price:20}," +
+                        "{id:4,name:Item4,price:30}]")) // we can pass in empty arrays aswell but the count should match like
+                .andReturn();                               // we can pass 1 array and add [],[] empty array it will still pass
+    }
 
 
 }
